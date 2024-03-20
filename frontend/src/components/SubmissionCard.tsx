b1db12a322e5197c6af9189b4696ca4c21b480c1
Npanda04 <deepanshusisodiya21@gmail.com>
2024-03-20 18:08:05 +0530

    added interfaces in frontend

--- frontend/src/components/SubmissionCard.tsx
+++ frontend/src/components/SubmissionCard.tsx
@@ -1,17 +1,25 @@
+import { TableBody, TableRow, TableCell } from "@/components/ui/table";
 
-import {  TableRow, TableCell, TableBody } from "@/components/ui/table"
-
-export default function TableComponent({language: string, username, stdin, sourcecode, outputcode}) {
-  return (
-      <TableBody>
-        <TableRow>
-          <TableCell className="font-medium">{username}</TableCell>
-          <TableCell>{language}</TableCell>
-          <TableCell>{stdin}</TableCell>
-          <TableCell>{sourcecode}</TableCell>
-          <TableCell>{outputcode}</TableCell>
-        </TableRow>
-      </TableBody>
-  )
+interface TableComponentProps {
+  language: string;
+  username: string;
+  stdin: string;
+  sourcecode: string;
+  outputcode: string;
 }
 
+const TableComponent: React.FC<TableComponentProps> = ({ language, username, stdin, sourcecode, outputcode }) => {
+  return (
+    <TableBody>
+      <TableRow>
+        <TableCell className="font-medium">{username}</TableCell>
+        <TableCell>{language}</TableCell>
+        <TableCell>{stdin}</TableCell>
+        <TableCell>{sourcecode}</TableCell>
+        <TableCell>{outputcode}</TableCell>
+      </TableRow>
+    </TableBody>
+  );
+};
+
+export default TableComponent;
